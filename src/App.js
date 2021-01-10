0acc0910fd1867e057070f2241a2f1bd8d92c29b
Jhonatan Eduardo <jhonatanepp@gmail.com>
2021-01-10 00:46:01 -0300

    add layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      playScore: 0,
+      start: false,
+      cartas: [
+        {
+          number: 2,
+          opened: false,
+          finded: false
+        },
+        {
+          number: 4,
+          opened: false,
+          finded: false
+        },
+        {
+          number: 3,
+          opened: false,
+          finded: false
+        },
+        {
+          number: 2,
+          opened: false,
+          finded: false
+        },
+        {
+          number: 1,
+          opened: false,
+          finded: false
+        },
+        {
+          number: 4,
+          opened: false,
+          finded: false
+        },
+        {
+          number: 1,
+          opened: false,
+          finded: false
+        },
+        {
+          number: 3,
+          opened: false,
+          finded: false
+        }
+      ]
+    }
+  }
+
+  playStart() {
+    const start = true
+    this.setState({ start })
+  }
+
+  cartaDisplay(index) {
+    const carta = this.state.cartas[index];
+    if (!this.state.start) { return; }
+    return (
+      <div className="carta" key={`carta-${index}`}>
+        <span>{carta.opened && carta.number}</span>
+      </div>
+    ) 
+  }
+
+  renderCartas() {
+    return this.state.cartas.map((carta, index) => this.cartaDisplay(index));
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Jogo da Memória</h1>
+        <hr/>
+        <div className="start">
+          <button onClick={() => this.playStart()}>Iniciar Jogo</button>
+        </div>
+        {this.renderCartas()}
+      </div>
+    )
+  }
+
 }
 
 export default App;
